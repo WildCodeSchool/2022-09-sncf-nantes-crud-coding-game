ffaf55dfb004fd6878e7d4a49eaf5f5091e60233
DamDamWCS <bobon.damien.wcs@gmail.com>
2023-03-17 10:09:49 +0100

    Route GET

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,8 +2,14 @@ const connection = require('../db-config');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-
-});
+    connection.query('SELECT * FROM product', (err, result) => {
+      if (err) {
+        res.status(500).send('Error retrieving animals from database');
+      } else {
+        res.json(result);
+      }
+    });
+  });
 
 router.get('/:id', (req, res) => {
 
