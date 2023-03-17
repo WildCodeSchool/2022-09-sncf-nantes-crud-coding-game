82afbd81a0bb15df17346fea31144ac38ceb0efe
DamDamWCS <bobon.damien.wcs@gmail.com>
2023-03-17 10:18:50 +0100

    Route GET by ID et Route POST

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,20 +4,46 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     connection.query('SELECT * FROM product', (err, result) => {
       if (err) {
-        res.status(500).send('Error retrieving animals from database');
+        res.status(500).send('Error retrieving products from database');
       } else {
         res.json(result);
       }
     });
   });
 
-router.get('/:id', (req, res) => {
+  router.get('/:id', (req, res) => {
+    const animalId = req.params.id;
+    connection.query(
+        'SELECT * FROM product WHERE id = ?',
+        [animalId],
+        (err, results) => {
+        if (err) {
+            res.status(500).send('Error retrieving product from database');
+        } else {
+            if (results.length) res.json(results[0]);
+            else res.status(404).send('Product not found');
+        }
+        }
+    );
+    }); 
 
-});
-
-router.post('/', (req, res) => {
-
-});
+    router.post('/', (req, res) => {
+        const { name, price, description, picture } = req.body;
+        connection.query(
+          'INSERT INTO product (name, price, description, picture) VALUES (?, ?, ?, ?)',
+          [name, price, description, picture],
+          (err, result) => {
+            if (err) {
+              console.error(err);
+              res.status(500).send('Error saving the product');
+            } else {
+              const id = result.insertId;
+              const createdProduct = { id, name, price, description, picture };
+              res.status(201).json(createdProduct);
+            }
+          }
+        );
+      });
 
 router.put('/:id', (req, res) => {
 
