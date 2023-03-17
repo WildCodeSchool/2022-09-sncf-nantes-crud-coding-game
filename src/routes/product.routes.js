9b7a65310d27c737c91e6afa0fb733488f2b28b0
BOSSE Nicolas (SNCF RESEAU / EIV DE SAINT DIZIER / EQUIP PROD UMV) <8011619g@commun.ad.sncf.fr>
2023-03-17 11:42:02 +0100

    allRoad

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -27,10 +27,59 @@ router.get("/:id", (req, res) => {
   );
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { name, species, age, description, picture } = req.body;
+  connection.query(
+    "INSERT INTO animal (name, species, age, description, picture) VALUES (?, ?, ?, ?, ?)",
+    [name, species, age, description, picture],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error saving the animal");
+      } else {
+        const id = result.insertId;
+        const createdAnimal = { id, name, species, age, description, picture };
+        res.status(201).json(createdAnimal);
+      }
+    }
+  );
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const animalId = req.params.id;
+  const db = connection.promise();
+  let existingAnimal = null;
+  db.query("SELECT * FROM animal WHERE id = ?", [animalId])
+    .then(([results]) => {
+      existingAnimal = results[0];
+      if (!existingAnimal) return Promise.reject("RECORD_NOT_FOUND");
+      return db.query("UPDATE animal SET ? WHERE id = ?", [req.body, animalId]);
+    })
+    .then(() => {
+      res.status(200).json({ ...existingAnimal, ...req.body });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err === "RECORD_NOT_FOUND")
+        res.status(404).send(`Animal with id ${animalId} not found.`);
+      else res.status(500).send("Error updating an animal");
+    });
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  connection.query(
+    "DELETE FROM animal WHERE id = ?",
+    [req.params.id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error deleting an animal");
+      } else {
+        if (result.affectedRows) res.status(200).send("🎉 Animal deleted!");
+        else res.status(404).send("Animal not found.");
+      }
+    }
+  );
+});
 
 module.exports = router;
