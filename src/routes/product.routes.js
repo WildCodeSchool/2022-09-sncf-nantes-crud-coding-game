60e2230a3203a58eb8331740a86aada0789bc1dd
margaux.begard <margaux.begard2@gmail.com>
2023-03-17 10:39:31 +0100

    route first done

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,24 +1,24 @@
-const connection = require('../db-config');
-const router = require('express').Router();
+const connection = require("../db-config");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM product", (err, result) => {
+    if (err) {
+      res
+        .status(500)
+        .send("Erreur pas de produit retrouvé dans la base de données");
+    } else {
+      res.json(result);
+    }
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.post("/", (req, res) => {});
 
-router.post('/', (req, res) => {
+router.put("/:id", (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
