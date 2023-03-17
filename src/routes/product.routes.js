eeb6b2181a8d7910812b4a8fc77cb9f270640ad8
BOSSE Nicolas (SNCF RESEAU / EIV DE SAINT DIZIER / EQUIP PROD UMV) <8011619g@commun.ad.sncf.fr>
2023-03-17 11:34:16 +0100

    routeGet

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,24 +1,36 @@
-const connection = require('../db-config');
-const router = require('express').Router();
+const connection = require("../db-config");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM animal", (err, result) => {
+    if (err) {
+      res.status(500).send("Error retrieving animals from database");
+    } else {
+      res.json(result);
+    }
+  });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  const animalId = req.params.id;
+  connection.query(
+    "SELECT * FROM animal WHERE id = ?",
+    [animalId],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving animal from database");
+      } else {
+        if (results.length) res.json(results[0]);
+        else res.status(404).send("Animal not found");
+      }
+    }
+  );
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.put("/:id", (req, res) => {});
 
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
