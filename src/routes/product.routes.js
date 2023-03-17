f72bdbbb4505b62f9d9d7e09d759aa5ab02d34ef
danyel duckman <danyel.duckman@gmail.com>
2023-03-17 11:50:52 +0100

    route delete ok

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -69,6 +69,20 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  connection.query(
+    "DELETE FROM product WHERE id = ?",
+    [req.params.id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error deleting an product");
+      } else {
+        if (result.affectedRows) res.status(200).send("🎉 Product deleted!");
+        else res.status(404).send("Product not found.");
+      }
+    }
+  );
+});
 
 module.exports = router;
